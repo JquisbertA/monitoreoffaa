a392cb530a881dc84acd8c41c6f25a308e10ff02
CARLOSLUZEDGAR <carlosyluzalberto@gmail.com>
2025-03-26 08:56:40 -0400

    260325_0825_inicio_desarrollo_configuracion

--- public/js/app.js
+++ public/js/app.js
@@ -54829,8 +54829,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vue_router__WEBPACK_IMPORTED_MODU
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\serbasfab\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\serbasfab\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\monitoreoffaa\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\monitoreoffaa\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
